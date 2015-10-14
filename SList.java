a06c1c85fe309021a84c2e551baa264f11b44fd8
aneesh0101 <aneesh.pagnis@zycus.com>
2015-10-14 20:02:37 +0530

    14oct2015

--- SList.java
+++ SList.java
@@ -1,48 +1,51 @@
 import java.io.*;
 
 class List {
-
-	public SListNode head;
-	public int size =0;
-
-	public List() {
-
-		head = null;
-		size = 0;
-	}
-
-	public void insertFront(int item) {
-
-		head = new SListNode(item, head);
-		size++;
-	}
-
-
+    
+    public ListNode head;
+    public int size;
+    
+    public List() {
+        
+        head = null;
+        size=0;
+    }
+    
+    public void insertFront(int i) {
+        
+        head = new ListNode(i, head);
+        size++;
+    }
+    
 }
 
-class SListNode {
-
-	public int item;
-	public SListNode next;
-
-	public SListNode(int i, SListNode s) {
-
-		item = i;
-		next = s;
-	}
-
-	public SListNode(int i) {
-
-		this(i,null);
-	}
+class ListNode {
+    
+    int item;
+    ListNode next;
+    
+    public ListNode (int i, ListNode s){
+        
+        item = i;
+        next = s;
+    }
+    
+    public ListNode (int i) {
+        
+        this(i,null);
+    }
 }
 
-public class SList {
-
-	public static void main(String[] args) {
-
-		List l = new List();
-		l.insertFront(45);
-		System.out.println(l.head.item)	;
-	}
+class SList {
+    
+    public static void main(String[] args) {
+        
+        List l1 = new List();
+        l1.insertFront(3);
+        l1.insertFront(4);
+        l1.insertFront(5);
+        System.out.println(l1.head.item);
+        System.out.println(l1.head.next.item);
+        System.out.println(l1.head.next.next.item);
+    }
 }
